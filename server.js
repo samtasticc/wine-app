3067ce51e22abbfcf93d113a3fac652c8d5cbe4a
samtasticc <samantha.henesy@gmail.com>
2024-09-30 20:47:24 -0400

    add server.js imports

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 // ========= IMPORTS ========= //
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+const methodOverride = require('method-override')
+const morgan = require('morgan')
 // ========= MONGOOSE ========= //
 // ========= MIDDLEWARE ========= //
 // ========= ROUTES ========= //
