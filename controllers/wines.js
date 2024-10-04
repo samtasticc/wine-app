52c3c1fd5c69fefba6607df45855d1839fa69068
samtasticc <samantha.henesy@gmail.com>
2024-10-04 09:55:12 -0400

    define post route in controller

--- controllers/wines.js
+++ controllers/wines.js
@@ -16,4 +16,16 @@ router.get('/', (req, res) => {
 router.get('/new', async (req, res) => {
     res.render('wines/new.ejs')
 })
+
+router.post('/', async (req, res) => {
+    try {
+        const currentUser = await User.findById(req.session.user._id)
+        currentUser.applications.push(req.body)
+        await currentUser.save()
+        res.redirect(`/users/${currentUser._id}/wines`)
+    } catch (error) {
+        console.log(error)
+        res.redirect('/')
+    }
+})
 module.exports = router
